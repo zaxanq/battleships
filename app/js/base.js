fa8203ff967e09ba8c429f87c52895ca9bd1e68e
zaxanq <zaxanq@gmail.com>
2019-06-14 13:54:53 +0200

    Fundamental change in labels rendering

--- app/js/base.js
+++ app/js/base.js
@@ -18,7 +18,20 @@ class Base {
         };
 
         Element.prototype.removeClass = function (className) {
-            this.classList.remove(className);
+            if (typeof className === 'string') {
+                this.classList.remove(className);
+            } else if (typeof className === 'object') {
+                if (className.length) {
+                    className.map((name) => {
+                        this.classList.remove(name);
+                    });
+                } else {
+                    console.error('Parameter needs to be a string or a array of strings');
+                }
+            } else {
+                console.error('Parameter needs to be a string or a array of strings');
+            }
+            return this;
             return this;
         };
     }
--- app/js/boards.js
+++ app/js/boards.js
@@ -27,9 +27,11 @@ class Boards extends Base {
         let board = document.createElement('div');
         board.addClass(['board', 'board-' + name]);
         this.main.append(board);
+
         this.Boards[name] = {name: name, DOM: {board: this.class('board-' + name)[0]}, active: false};
 
         this.createFields(this.Boards[name]);
+        this.createLabels(this.Boards[name]);
 
         if (name === 'player') {
             this.shipHolder = this.createShipHolder();
@@ -37,17 +39,35 @@ class Boards extends Base {
     }
 
     createFields(board) {
-        let letters = 'ABCDEFGHIJ';
+        this.letters = 'ABCDEFGHIJ';
         board.field = {};
         for (let i = 0; i < 10; i++) {
-            let row = document.createElement('div').addClass(['row', `row-${letters[i]}`]);
+            let row = document.createElement('div').addClass(['row', `row-${this.letters[i]}`]);
             board.DOM.board.append(row);
             for (let j = 1; j <= 10; j++) {
-                board.field[`${letters[i]}${j}`] = this.fieldState[0];
-                let field = document.createElement('div').addClass(['field', this.fieldClasses['empty'], board.name, `${letters[i]}${j}`]);
+                board.field[`${this.letters[i]}${j}`] = this.fieldState[0];
+                let field = document.createElement('div').addClass(['field', this.fieldClasses['empty'], board.name, `${this.letters[i]}${j}`]);
                 row.append(field);
             }
         }
+    }
+
+    createLabels(board) {
+        let horizontalLabels = document.createElement('div').addClass('label-container--horizontal');
+        let verticalLabels = document.createElement('div').addClass('label-container--vertical');
+        board.DOM.board.append(horizontalLabels);
+        board.DOM.board.append(verticalLabels);
+
+        for (let i = 0; i < 10; i++) {
+            let horizontalLabel = document.createElement('div').addClass('label');
+            horizontalLabel.innerText = this.letters[i];
+            horizontalLabels.append(horizontalLabel);
+
+            let verticalLabel = document.createElement('div').addClass('label');
+            verticalLabel.innerText = i+1;
+            verticalLabels.append(verticalLabel);
+        }
+
     }
 
     createShipHolder() {
